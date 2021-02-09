faa63585356d3afe0ea9fc9fee4704608dd2d6e5
denmanj <denman.j@northeastern.edu>
2021-02-08 20:15:57 -0500

    Added create, delete, and update user functions

--- src/main/webapp/user-admin/user-admin.controller.client.js
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -1,12 +1,12 @@
 var $usernameFld, $passwordFld;
 var $firstNameFld, $lastNameFld, $roleFld;
-var $removeBtn, $editBtn, $createBtn;
+var $deleteBtn, $updateBtn, $createBtn;
 var $userRowTemplate, $tbody;
-//var userService = new AdminUserServiceClient();
+var userService = new AdminUserServiceClient();
 
 var users = [
-    {username: "usrnme", firstname: "first", lastname: "last", role: "FACULTY"},
-    {username: "usrname", firstname: "frst", lastname: "lst", role: "STUDENT"},
+    // {username: "usrnme", firstname: "first", lastname: "last", role: "FACULTY"},
+    // {username: "usrname", firstname: "frst", lastname: "lst", role: "STUDENT"},
 ]
 
 function renderUsers(users) {
@@ -22,19 +22,85 @@ function renderUsers(users) {
                 <td>${user.lastname}</td>
                 <td>${user.role}</td>
                 <td class="pull-right" style="white-space: nowrap">
-                    <button id="${i} class="wbdv-delete-btn">Delete</button>
-                    <button class="wbdv-select-btn">Select</button>
+                    <button id="${i}" class="wbdv-delete-btn">Delete</button>
+                    <button id="${user._id}" class="wbdv-select-btn">Select</button>
                 </td>
             </tr>
             `)
     }
-    // $(".wbdv-delete-btn").click(deleteUser)
-    // $(".wbdv-select-btn").click(selectUser)
+    $(".wbdv-delete-btn").click(deleteUser)
+    $(".wbdv-select-btn").click(selectUser)
+}
+
+function createUser() {
+    var newUser = {
+        username: $usernameFld.val(),
+        firstname: $usernameFld.val(),
+        lastname: $lastNameFld.val(),
+        role: $roleFld.val()
+    }
+
+    userService.createUser(newUser)
+        .then(function (actualUser) {
+            users.push(actualUser)
+            renderUsers(users)
+        })
+}
+
+function deleteUser(event) {
+    alert("delete user")
+    var button = $(event.target)
+    var index = button.attr("id")
+    var id = users[index]._id
+    userService.deleteUser(id)
+        .then(function (status) {
+            users.splice(index, 1)
+            renderUsers(users)
+        })
+}
+
+var selectedUser = null
+function selectUser(event) {
+    var id = $(event.target).attr("id")
+    console.log(id)
+    selectedUser = users.find(user => user._id === id)
+    $usernameFld.val(selectedUser.username)
+    $firstNameFld.val(selectedUser.firstname)
+    $lastNameFld.val(selectedUser.lastname)
+    $roleFld.val(selectedUser.role)
+}
+
+function updateUser() {
+    selectedUser.username = $usernameFld.val()
+    selectedUser.firstname = $firstNameFld.val()
+    selectedUser.lastname = $lastNameFld.val()
+    selectedUser.role = $roleFld.val()
+    userService.updateUser(selectedUser._id, selectedUser)
+        .then(status => {
+            var index = users.findIndex(user => user._id === selectedUser._id)
+            users[index] = selectedUser
+            renderUsers(users)
+        })
 }
 
 function main() {
     $userRowTemplate = $(".wbdv-tbody")
+    $createBtn = $(".wbdv-create-btn")
+    $updateBtn = $(".wbdv-update-btn")
+
+    $usernameFld = $(".wbdv-username-fld")
+    $firstNameFld = $(".wbdv-firstname-fld")
+    $lastNameFld = $(".wbdv-lastname-fld")
+    $roleFld = $(".wbdv-role-fld")
 
     renderUsers(users)
+
+    $updateBtn.click(updateUser)
+    $createBtn.click(createUser)
+
+    userService.findAllUsers().then(function (actualUsers) {
+        users = actualUsers
+        renderUsers(users)
+    })
 }
 $(main)
